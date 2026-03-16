d4105a47b80cfb83b308d711b543f343399fa52c
Fatratra <nyfatratrar@gmail.com>
2026-03-16 16:47:14 +0300

    ref:ui improvement and refresh navbar

--- frontend/public/main.js
+++ frontend/public/main.js
@@ -1,9 +1,15 @@
-import { renderNavLinks, initCart } from "./js/navbar.js";
-import { initHeroSection, initAboutSection, initHomeCoursesSection, initExperienceSection, initCtaBanner } from "./js/pages/home.js";
-import { initCoursesPage }      from "./js/pages/courses.js";
-import { initBlogPage }         from "./js/pages/blog.js";
+import { renderNavLinks, initCart, initHamburger } from "./js/navbar.js";
+import {
+  initHeroSection,
+  initAboutSection,
+  initHomeCoursesSection,
+  initExperienceSection,
+  initCtaBanner,
+} from "./js/pages/home.js";
+import { initCoursesPage } from "./js/pages/courses.js";
+import { initBlogPage } from "./js/pages/blog.js";
 import { initTestimonialsPage } from "./js/pages/testimonials.js";
-import { initResearchPage }     from "./js/pages/research.js";
+import { initResearchPage } from "./js/pages/research.js";
 
 const PAGE_INIT = {
   "index.html": () => {
@@ -13,15 +19,16 @@ const PAGE_INIT = {
     initExperienceSection();
     initCtaBanner();
   },
-  "courses.html":      initCoursesPage,
-  "blog.html":         initBlogPage,
+  "courses.html": initCoursesPage,
+  "blog.html": initBlogPage,
   "testimonials.html": initTestimonialsPage,
-  "research.html":     initResearchPage,
+  "research.html": initResearchPage,
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   renderNavLinks();
   initCart();
+  initHamburger();
 
   const page = location.pathname.split("/").pop() || "index.html";
   PAGE_INIT[page]?.();
